2a0f246ddeed036c44985d7b05734f6ca99d9315
nick-w-nick <43578531+nick-w-nick@users.noreply.github.com>
2020-10-13 14:43:48 -0400

    init for scripts + createBulkGroups [WIP]

---
+++ categories/scripts/Create Groups in Bulk/createBulkGroups.js
@@ -0,0 +1,67 @@
+const groupJSON = require('./groups-to-create.json');
+const Axios = require('axios').default;
+
+const salsifyOrgId = process.env.salsifyOrgId;
+const salsifyToken = process.env.salsifyToken;
+
+const salsifyURL = `https://app.salsify.com/api/orgs/${salsifyOrgId}/groups`;
+
+const createGroup = async (groupName) => {
+	const requestData = {
+		data: {
+			name: groupName,
+			action_permissions: [
+				{
+					entity: 'Product',
+					actions: ['read'],
+				},
+				{
+					entity: 'ProfilesController',
+					actions: ['manage'],
+				},
+				{
+					entity: 'Property',
+					actions: ['read'],
+				},
+				{
+					entity: 'List',
+					actions: ['read'],
+				},
+				{
+					entity: 'WorkflowTask',
+					actions: ['read'],
+				},
+				{
+					entity: 'DigitalAsset',
+					actions: ['read'],
+				},
+			],
+		},
+    };
+    
+    const config = {
+        headers: {
+            Authorization: `Bearer ${salsifyToken}`
+        }
+    };
+    
+    try {
+        await Axios.post(salsifyURL, requestData, config);
+        return {
+            groupName: groupName,
+            status: `success`,
+            errors: null
+        }
+    } catch (error) {
+        return {
+            groupName: groupName,
+            status: 'error',
+            errors: error.message
+        }
+    }
+};
+
+groupJSON.forEach(async group => {
+    const response = await createGroup(group);
+    console.log(`Group Name: ${response.groupName}\nStatus: ${response.status}\n${response.errors ? `Errors: ${response.errors}\n` : ''}`)
+});
